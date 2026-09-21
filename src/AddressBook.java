02bd003343c640f5ec20230bf9b86e70f45cfdac
loude <louderluke@gmail.com>
2026-09-21 16:43:32 -0400

    added bud 4

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,11 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddressBook {
-    BuddyInfo bud1 = new BuddyInfo("bud1");
+    List<BuddyInfo> buddyList = new ArrayList();
 
-    BuddyInfo bud2 = new BuddyInfo("bud2");
+    public void addBuddy(BuddyInfo bud) {
+        buddyList.add(bud);
+    }
 
-    BuddyInfo bud3 = new BuddyInfo("bud3");
+    public void removeBuddy(BuddyInfo bud) {
+        buddyList.remove(bud);
+    }
 
     public static void main(String[] args){
         System.out.println("address book");
+
+        BuddyInfo bud4 = new BuddyInfo("bud2");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(bud4);
+        addressBook.removeBuddy(bud4);
     }
 }
