d85362650ca9dc1041d0cd579465bfc9d7976ad1
loude <louderluke@gmail.com>
2026-09-21 16:33:35 -0400

    added a print line

---
+++ src/AddressBook.java
@@ -0,0 +1,11 @@
+public class AddressBook {
+    BuddyInfo bud1 = new BuddyInfo("bud1");
+
+    BuddyInfo bud2 = new BuddyInfo("bud2");
+
+    BuddyInfo bud3 = new BuddyInfo("bud3");
+
+    public static void main(String[] args){
+        System.out.println("address book");
+    }
+}
