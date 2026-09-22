542ebfc1b668d6939cb15d95343280589f6b28ea
loude <louderluke@gmail.com>
2026-09-22 10:53:26 -0400

    added a print line

---
+++ src/BuddyInfo.java
@@ -0,0 +1,21 @@
+public class BuddyInfo {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public BuddyInfo(String name) {
+        this.name = name;
+    }
+
+    public BuddyInfo() {
+        this.name = "Place Holder";
+    }
+
+    public static void main(String[] args) {
+        BuddyInfo bud = new BuddyInfo("Tom");
+        System.out.println("Hello world! " + bud.getName());
+        System.out.println("test");
+    }
+}
